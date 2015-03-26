f214128b4b6f84fdbedabcd3c60ecf1ce5a1cebd
Brian Urry <urrry1@gmail.com>
2015-03-25 22:41:19 -0600

    Adjusted following cushion on hitNextCar().  We will likely want to reduce the time again in the future, however.

--- simulator_0.0/src/simulator/models/CarManager.java
+++ simulator_0.0/src/simulator/models/CarManager.java
@@ -14,7 +14,7 @@ public class CarManager implements Comparable {
 	
 	public static final double CAR_CUSHION = 10.0; //in meters
 	public static final double CAR_STOP_CUSHION = 7.5; //in meters
-	public static final double TIME_CUSHION = 0.5;
+	public static final double TIME_CUSHION = 1.5;
 	
 	private double arrivalTime;
 	private Car car;
@@ -158,7 +158,7 @@ public class CarManager implements Comparable {
 		if(nextCar != null && nextCar.getPosition() == this.car.getPosition()){//You're the only car on the road!
 			return false;
 		}
-		else if(nextCar != null && nextCar.getTimeTo(nextCar.targetSpeed, distanceToLight) > theoreticalTimeToLight + TIME_CUSHION){
+		else if(nextCar != null && nextCar.getTimeTo(nextCar.targetSpeed, distanceToLight) > theoreticalTimeToLight - TIME_CUSHION){
 			return true;
 		}
 		//if there is no next carr return false? The above if() checks if you're the only car on the stretch of road
