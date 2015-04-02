c09f2b6a21319490562a2ae0020552da468aa911
Brian Urry <urrry1@gmail.com>
2015-04-01 18:00:06 -0600

    Added missing function to phase2 stop light.

--- simulator_0.0/src/simulator/models/stoplights/Phase2StopLight.java
+++ simulator_0.0/src/simulator/models/stoplights/Phase2StopLight.java
@@ -1,12 +1,36 @@
 package simulator.models.stoplights;
 
+import simulator.models.stoplights.StopLight.Color;
+import simulator.outputter.Outputter;
+import simulator.phases.PhaseHandler;
+
 public class Phase2StopLight extends Phase1StopLight {
 
 	public Phase2StopLight(String configString) {
 		super(configString);
 	}
 	
-	@Override
+	@Override	
+	public void handleLightColors(double timePassed, PhaseHandler phase) {
+		timeUntilColorChange-=timePassed;
+		if(timeUntilColorChange < 0) {
+			if(this.currentColor == Color.GREEN) {
+				//this.currentColor = Color.RED;
+				//this.timeUntilColorChange = this.timeAsRed;
+				setTimeUntilColorChange();
+				Outputter.getOutputter().addLightOutput(this);
+			} else {
+				//this.currentColor = Color.GREEN;
+				//this.timeUntilColorChange = this.timeAsGreen;
+				setTimeUntilColorChange();
+				Outputter.getOutputter().addLightOutput(this);
+				
+				//Call the algorithm on current cars to catch rounding errors on cars approaching the newly green light
+//				this.CallIntermediateAlgorithmOnAllCars(phase); //TODO: Make this actually work! Currently broken because of faulty lane changes between lights
+			}
+		}
+	}
+
 	protected void setTimeUntilColorChange() {
 		lightTimes.set(0, lightTimes.get(0)-1);
 		if(lightTimes.get(0) <= 0){//change color
